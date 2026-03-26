5bf86ec46dfe8a341b2c9fd174c9af344775295f
ssalguero <104514487+ssalguero@users.noreply.github.com>
2026-03-26 18:54:21 -0300

    Carga inicial de niveles 31-40

---
+++ EduQuest_App/sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'eduaiti-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+  );
+  self.skipWaiting(); // Obliga al service worker nuevo a activarse rápido
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
